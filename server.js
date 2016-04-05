ea4fb3af6b3f968eb467c47d8e7d11c3bafbee73
Ryan Mulcahy <ryan@primeacademy.io>
2016-04-05 16:07:07 -0500

    Updates from second lecture.
    Update the server to demo POST requests. Demo serving static Assests.

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 var express = require('express');
-var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 var port = 3000;
 
-app.listen(port, function(){
-  console.log('Listening for requests on port:' + port);
-});
+// where we will store data that is POSTed to the server
+var data = [];
 
-function handleGet(req, res) {
-  console.log('received a request!');
-  res.sendFile(path.join(__dirname, './index.html'));
-}
+// add the bodyParser middleware
+// this gives us access to the req.body object,
+// which will contain the body data from the HTTP request itself
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-// http://localhost:port/
-app.get('/', handleGet);
+// serve static files from the public directory
+//
+// note: if the server recieves a GET request for '/'
+// *AND* an index.html file exists, it will be served
+// ex. GET localhost:3000 -> index.html
+app.use(express.static('public'));
 
+// A route for handling POST requests to the root URL
 app.post('/', function(req, res) {
-  console.log('received a post request!');
-  res.sendStatus(204);
+  console.log('received a post request! req.body', req.body);
+  data.push(req.body);
+  console.log('contents of data=', data);
+  res.send(data);
 });
 
+// A route for handling GET requests to '/cats'
 app.get('/cats', function(req, res) {
   res.send('meow');
+});
+
+// Tell the server to listen for requests on the given port
+app.listen(port, function(){
+  console.log('Listening for requests on port:', port);
 });
